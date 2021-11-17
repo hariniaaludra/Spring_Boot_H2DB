601a97066738c55927961da7cc58a0a1a2fcbee9
Aswathymanikandan <aswathy26797@gmail.com>
2021-11-17 17:35:22 +0530

    user

---
+++ src/main/java/com/aaludra/spring/jpa/h2/model/User.java
@@ -0,0 +1,50 @@
+package com.aaludra.spring.jpa.h2.model;
+
+import java.sql.Date;
+
+public class User {
+	private int id;
+	private String username;
+    private String displayname;
+    private String password;
+	private Date dob;
+	private int phoneno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getDisplayname() {
+		return displayname;
+	}
+	public void setDisplayname(String displayname) {
+		this.displayname = displayname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public int getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(int phoneno) {
+		this.phoneno = phoneno;
+	}
+}
+	
+		
