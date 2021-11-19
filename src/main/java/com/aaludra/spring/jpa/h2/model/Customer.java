da2cc2d868c8812f84c347b8cdff350972540797
Gladsonraj <gladsonraj19@gmail.com>
2021-11-19 10:57:19 +0530

    customer

--- src/main/java/com/aaludra/spring/jpa/h2/model/Customer.java
+++ src/main/java/com/aaludra/spring/jpa/h2/model/Customer.java
@@ -5,7 +5,7 @@ import java.sql.Date;
 import javax.persistence.*;
 
 @Entity
-@Table (name = "Customer")
+@Table (name = "Customer") 
 public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
