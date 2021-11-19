e81fa2deae3487983391762c1450018b53cadaff
Gladsonraj <gladsonraj19@gmail.com>
2021-11-19 10:23:03 +0530

    customer

--- src/main/java/com/aaludra/spring/jpa/h2/model/Customer.java
+++ src/main/java/com/aaludra/spring/jpa/h2/model/Customer.java
@@ -1,16 +1,42 @@
 package com.aaludra.spring.jpa.h2.model;
 
+import java.security.Timestamp;
 import java.sql.Date;
 
 import javax.persistence.*;
 
+@Entity
+@Table(name = "Customer")
 public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	@Column(name = "cust_name")
 	private String cust_name;
+	
+	@Column(name = "cust_Id")
 	private long cust_Id;
+	
+	@Column(name = "city")
 	private String city;
+	
+	@Column(name = "dob")
 	private Date dob;
+	
+	@Column(name = "Gstin")
 	private String Gstin;
+	
+	public Customer(long id, String cust_name, long cust_Id, String city, Date dob, String Gstin) {
+		this.id=id;
+		this.cust_name=cust_name;
+		this.cust_Id=cust_Id;
+		this.city=city;
+		this.dob=dob;
+		this.Gstin=Gstin;
+		
+	}
 
 
 	public long getId() {
@@ -61,4 +87,15 @@ public class Customer {
 	public void setGstin(String gstin) {
 		Gstin = gstin;
 	}
+
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", cust_name=" + cust_name + ", cust_Id=" + cust_Id + ", city=" + city + ", dob="
+				+ dob + ", Gstin=" + Gstin + ", getId()=" + getId() + ", getCust_name()=" + getCust_name()
+				+ ", getCust_id()=" + getCust_id() + ", getCity()=" + getCity() + ", getDob()=" + getDob()
+				+ ", getGstin()=" + getGstin() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
 }
