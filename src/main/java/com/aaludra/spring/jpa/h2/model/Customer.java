2a4d770db52e6d00d8e9bf42c12dc8d0dfbc46f0
Gladsonraj <gladsonraj19@gmail.com>
2021-11-22 11:08:41 +0530

    adding fields in customer

--- src/main/java/com/aaludra/spring/jpa/h2/model/Customer.java
+++ src/main/java/com/aaludra/spring/jpa/h2/model/Customer.java
@@ -1,5 +1,6 @@
 package com.aaludra.spring.jpa.h2.model;
 
+import java.security.Timestamp;
 import java.sql.Date;
 
 import javax.persistence.*;
@@ -26,15 +27,34 @@ public class Customer {
 	@Column(name = "Gstin")
 	private String Gstin;
 	
-	public Customer(long id, String cust_name,long cust_Id, String city,Date dob,String Gstin) {
+	@Column(name = "status")
+	private String status;
+	
+	@Column(name = "created_by")
+	private String createdBy;
+	
+	@Column(name = "created_date")
+	private Timestamp createdDate;
+	
+	@Column(name = "updated_by")
+	private String updatedBy;
+	
+	@Column(name = "updated_date")
+	private Timestamp updatedDate;
+	
+	public Customer(long id, String cust_name,long cust_Id, String city,Date dob,String Gstin,String status,String createdBy,Timestamp createdDate,String updatedBy,Timestamp updatedDate) {
 		this.id=id;
 		this.cust_name=cust_name;
 		this.cust_Id=cust_Id;
 		this.city=city;
 		this.dob=dob;
 		this.Gstin=Gstin;
+		this.status=status;
+		this.createdBy=createdBy;
+		this.createdDate=createdDate;
+		this.updatedBy=updatedBy;
+		this.updatedDate=updatedDate;
 	}
-
 
 	public long getId() {
 		return id;
@@ -44,7 +64,6 @@ public class Customer {
 		this.id = id;
 	}
 
-	
 	public String getCust_name() {
 		return cust_name;
 	}
@@ -53,11 +72,11 @@ public class Customer {
 		this.cust_name = cust_name;
 	}
 
-	public long getCust_id() {
+	public long getCust_Id() {
 		return cust_Id;
 	}
 
-	public void setCust_id(long cust_Id) {
+	public void setCust_Id(long cust_Id) {
 		this.cust_Id = cust_Id;
 	}
 
@@ -85,13 +104,53 @@ public class Customer {
 		Gstin = gstin;
 	}
 
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+
+	public Timestamp getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Timestamp updatedDate) {
+		this.updatedDate = updatedDate;
+	}
 
 	@Override
 	public String toString() {
 		return "Customer [id=" + id + ", cust_name=" + cust_name + ", cust_Id=" + cust_Id + ", city=" + city + ", dob="
-				+ dob + ", Gstin=" + Gstin + "]";
+				+ dob + ", Gstin=" + Gstin + ", status=" + status + ", createdBy=" + createdBy + ", createdDate="
+				+ createdDate + ", updatedBy=" + updatedBy + ", updatedDate=" + updatedDate + "]";
 	}
 
-	
+
 	
 }
