016d0f779c23367763e7be7278a4f33338e4989d
harinirswekrish <harinirswekrish@gmail.com>
2021-12-10 15:32:04 +0530

    Studentinput

---
+++ src/main/java/com/aaludra/spring/jpa/h2/view/Studentxml.java
@@ -0,0 +1,115 @@
+package com.aaludra.spring.jpa.h2.view;
+
+public class Studentxml {
+	private String studentname;
+	private String rollnumber;
+	private String course;
+	private String degree;
+	private String dob;
+	private String status;
+	private String createdby;
+	private String createddate;
+	private String updatedby;
+	private String updateddate;
+
+	public Studentxml() {
+		super();
+
+	}
+
+	public Studentxml(String studentname, String rollnumber, String course, String degree, String dob, String status,
+			String createdby, String createddate, String updatedby, String updateddate) {
+		super();
+		this.studentname = studentname;
+		this.rollnumber = rollnumber;
+		this.course = course;
+		this.degree = degree;
+		this.dob = dob;
+		this.status = status;
+		this.createdby = createdby;
+		this.createddate = createddate;
+		this.updatedby = updatedby;
+		this.updateddate = updateddate;
+	}
+
+	public String getStudentname() {
+		return studentname;
+	}
+
+	public void setStudentname(String studentname) {
+		this.studentname = studentname;
+	}
+
+	public String getRollnumber() {
+		return rollnumber;
+	}
+
+	public void setRollnumber(String rollnumber) {
+		this.rollnumber = rollnumber;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String getDegree() {
+		return degree;
+	}
+
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCreatedby() {
+		return createdby;
+	}
+
+	public void setCreatedby(String createdby) {
+		this.createdby = createdby;
+	}
+
+	public String getCreateddate() {
+		return createddate;
+	}
+
+	public void setCreateddate(String createddate) {
+		this.createddate = createddate;
+	}
+
+	public String getUpdatedby() {
+		return updatedby;
+	}
+
+	public void setUpdatedby(String updatedby) {
+		this.updatedby = updatedby;
+	}
+
+	public String getUpdateddate() {
+		return updateddate;
+	}
+
+	public void setUpdateddate(String updateddate) {
+		this.updateddate = updateddate;
+	}
+
+}
